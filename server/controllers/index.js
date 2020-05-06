ba44690e7ba9fed1babc97b0c49c04f76eba2969
TheJang <wkddnjs7310@naver.com>
2020-05-06 17:23:24 +0900

    promise ing

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -4,39 +4,27 @@ var models = require("../models");
 
 module.exports = {
   messages: {
-    get: function (req, res) {
-      models.messages.get((error, data) => {
-        if (error) {
-          res.send(error);
-        }
-        res.status(200).send(JSON.stringify(data));
+    get: async function (req, res) {
+      await models.messages.get().then((data) => {
+        res.status(200).send(data);
       });
     },
-    post: function (req, res) {
-      models.messages.post(req.body, (error, data) => {
-        if (error) {
-          res.send(error);
-        }
-        res.status(200).send(JSON.stringify(data));
+    post: async function (req, res) {
+      await models.messages.post(req.body).then((data) => {
+        res.status(200).send(data);
       });
     },
   },
 
   users: {
-    get: function (req, res) {
-      models.users.get((error, data) => {
-        if (error) {
-          res.send(error);
-        }
-        res.status(200).send(JSON.stringify(data));
+    get: async function (req, res) {
+      await models.messages.get().then((data) => {
+        res.status(200).send(data);
       });
     },
-    post: function (req, res) {
-      models.users.post(req.body, (error, data) => {
-        if (error) {
-          res.send(error);
-        }
-        res.status(200).send(JSON.stringify(data));
+    post: async function (req, res) {
+      await models.messages.post(req.body).then((data) => {
+        res.status(200).send(data);
       });
     },
   },
--- server/models/index.js
+++ server/models/index.js
@@ -3,55 +3,51 @@ var db = require("../db");
 
 module.exports = {
   messages: {
-    get: function (callback) {
-      db.query("SELECT * FROM messages", (error, data) => {
-        if (error) {
-          callback(error);
-        }
-        console.log(data);
-        callback(null, data);
+    get: function () {
+      return new Promise((resolve, reject) => {
+        let sql = "SELECT * FROM messages";
+        db.query(sql, (err, data) => {
+          if (err) reject(err);
+          resolve(data);
+        });
       });
     },
-    post: function ({ username, text, roomname }, callback) {
+    post: function ({ username, text, roomname }) {
       let sql = `INSERT INTO messages(username, text, roomname) VALUES (?,?,?)`;
       let params = [username, text, roomname];
-      db.query(sql, params, (error) => {
-        if (error) {
-          callback(error);
-        }
-        db.query("SELECT * FROM messages", params, (err, data) => {
-          if (err) {
-            callback(err);
-          }
-          callback(null, data);
+      return new Promise((resolve, reject) => {
+        db.query(sql, params, (err) => {
+          if (err) reject(err);
+          db.query("SELECT * FROM messages", (err, data) => {
+            if (err) reject(err);
+            resolve(data);
+          });
         });
       });
     },
   },
-  // 잔디 확인
 
   users: {
     // Ditto as above.
-    get: function (callback) {
-      db.query("SELECT * FROM users", (error, data) => {
-        if (error) {
-          callback(error);
-        }
-        callback(null, data);
+    get: function () {
+      return new Promise((resolve, reject) => {
+        let sql = "SELECT * FROM users";
+        db.query(sql, (err, data) => {
+          if (err) reject(err);
+          resolve(data);
+        });
       });
     },
-    post: function ({ username }, callback) {
-      let sql = `INSERT INTO users(username) VALUES(?)`;
+    post: function ({ username }) {
+      let sql = `INSERT INTO users(username) VALUES (?)`;
       let params = [username];
-      db.query(sql, params, (error) => {
-        if (error) {
-          callback(error);
-        }
-        db.query("SELECT * FROM users", params, (err, data) => {
-          if (err) {
-            callback(err);
-          }
-          callback(null, data);
+      return new Promise((resolve, reject) => {
+        db.query(sql, params, (err) => {
+          if (err) reject(err);
+          db.query("SELECT * FROM users", (err, data) => {
+            if (err) reject(err);
+            resolve(data);
+          });
         });
       });
     },
